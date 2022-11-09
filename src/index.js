f16810b809312840631971be26bb5d05102a0570
Reynaldo Cerpa <reynaldocerpa07@gmail.com>
2022-11-08 23:08:59 -0800

    encapsulated board

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import BoardView from "./components/Board";
+import Home from './pages/Home.jsx'
 import "./main.scss";
 import "./styles.scss";
 
 const App = () => {
-  return <BoardView />;
+  return <Home />;
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <h1>2048</h1>
     <App />
   </>
 );
