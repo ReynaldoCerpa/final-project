4c4056d8ce77c30db0ea3cbc23936a50257f29e4
Reynaldo Cerpa <reynaldocerpa07@gmail.com>
2022-11-09 22:37:11 -0800

    remove extension names

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar'
-import Home from './pages/Home.jsx'
-import Leaderboard from './pages/Leaderboard.jsx'
+import Home from './pages/Home'
+import Leaderboard from './pages/Leaderboard'
 import "./main.scss";
 import "./styles.scss";
 
